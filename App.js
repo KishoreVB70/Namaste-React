da224262a1808b4457d025698718e7d8fc3aaa24
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-20 12:36:00 +0530

    Code to render nested elements

--- App.js
+++ App.js
@@ -1,3 +1,14 @@
-const heading = React.createElement("h1", {}, "H World");
+// Return react object
+const heading = React.createElement("h1", {id: "heading"}, "H World");
+
+
+const child = React.createElement("div", {id: "child"}, heading);
+const parent = React.createElement("div", {id: "parent"}, child);
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+console.log(heading);
+console.log(root);
+
+// Convert the heading object into heading HTML element, and render the DOM
+root.render(parent);
