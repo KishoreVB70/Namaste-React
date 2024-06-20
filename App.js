bfd7f43cb4baf1bf4afed81608165ed58474ab44
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-20 12:42:43 +0530

    Added multiple children for a single parent element

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 // Return react object
-const heading = React.createElement("h1", {id: "heading"}, "H World");
+const heading1 = React.createElement("h1", {id: "heading1"}, "H One");
+const heading2 = React.createElement("h1", {id: "heading2"}, "H Two");
 
 
-const child = React.createElement("div", {id: "child"}, heading);
-const parent = React.createElement("div", {id: "parent"}, child);
+const child1 = React.createElement("div", {id: "child1"}, heading1);
+const child2 = React.createElement("div", {id: "child2"}, heading2);
+
+const parent = React.createElement("div", {id: "parent"}, [child1, child2]);
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(heading);
-console.log(root);
+console.log(parent);
 
 // Convert the heading object into heading HTML element, and render the DOM
 root.render(parent);
