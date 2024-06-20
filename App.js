586f79b42b94c0fa120ba4fbb559f89a639ead42
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-20 14:43:08 +0530

    Added NPM

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 // Return react object
 const heading1 = React.createElement("h1", {id: "heading1"}, "H One");
-const heading2 = React.createElement("h1", {id: "heading2"}, "H Two");
-
+const heading2 = React.createElement("h2", {id: "heading2"}, "H Two");
 
 const child1 = React.createElement("div", {id: "child1"}, heading1);
 const child2 = React.createElement("div", {id: "child2"}, heading2);
@@ -10,7 +9,5 @@ const parent = React.createElement("div", {id: "parent"}, [child1, child2]);
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(parent);
-
 // Convert the heading object into heading HTML element, and render the DOM
 root.render(parent);
