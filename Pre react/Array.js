c39b33e9aefd1900184895bb56627f72bb83cc65
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-20 10:17:08 +0530

    Code for spread operator

--- Pre react/Array.js
+++ Pre react/Array.js
@@ -1,17 +1,25 @@
-const arr = ["1", 1, 1.1, true];
+let arr = ["1", 1, 1.1, true];
 arr[5] = "Jesse";
 
-// For of loop
-// i will will take the value of each element of the array
-// Include undefined
-for (i of arr) {
-    console.log(i);
+function ofinLoops() {
+    // For of loop
+    // i will will take the value of each element of the array
+    // Include undefined
+    for (i of arr) {
+        console.log(i);
+    }
+
+    // For in loop
+    // i will take the array index
+    // Doesn't include undefined
+    for (i in arr) {
+        console.log(arr[i]);
+    }
 }
 
-// For in loop
-// i will take the array index
-// Doesn't include undefined
-for (i in arr) {
-    console.log(arr[i]);
-}
 
+
+// Spread operator
+let arr1 = [1,2];
+arr = [...arr, ...arr1];
+console.log(arr);
