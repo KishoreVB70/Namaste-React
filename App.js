3ba9ff838ff8002b553aa552b31366def6c98a79
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-21 10:51:54 +0530

    Used JS expression inside JSX

--- App.js
+++ App.js
@@ -4,11 +4,16 @@ import ReactDOM from "react-dom/client";
 // Create React elements(Object) - Not HTML DOM element
 // Return react object 
 // React Functional component -> Return JSXs
-        
-const Heading = () => <h1 id="h0" >I'm from functional component</h1>;
+let a = "Habibi welcome to puffy Kooty"
+
+const Heading = () => <h1 id="h0" >{a}</h1>;
+
 const JsxDiv = () => (
     <div>
         <Heading/>
+        {
+            "Habiibii"
+        }
     </div>
 );
 
