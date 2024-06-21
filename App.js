a83e1e021155f01f2cb7997229a2eba2d886223f
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-21 08:24:09 +0530

    Added JSX along with react elements

--- App.js
+++ App.js
@@ -1,15 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// Return react object
-const heading1 = React.createElement("h1", {id: "heading1"}, "H One");
-const heading2 = React.createElement("h2", {id: "heading2"}, "H Two");
 
-const child1 = React.createElement("div", {id: "child1"}, heading1);
-const child2 = React.createElement("div", {id: "child2"}, heading2);
+
+
+
+// Create React elements(Object) - Not HTML DOM element
+// Return react object 
+const NormalHeading = React.createElement("h2", {id: "heading1"}, "H One");
+
+// This also Returns the same React element as others
+const jsxDiv =  
+<div>
+    <h1 id="heading0" >I'm JSX bro</h1>
+</div>
+
+const child1 = React.createElement("div", {id: "child1"}, NormalHeading);
+const child2 = React.createElement("div", {id: "child2"}, jsxDiv);
 
 const parent = React.createElement("div", {id: "parent"}, [child1, child2]);
 
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// Convert the heading object into heading HTML element, and render the DOM
+
+// Convert the React element into heading HTML DOM element, and render the DOM
 root.render(parent);
